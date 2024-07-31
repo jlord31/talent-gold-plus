521c9a9080df7161a44e6a2789b676bd1373148b
jlord31 <adepoju97@gmail.com>
2024-07-31 15:50:21 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,126 @@
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
+});
+
+/*-------------------------------------
+|    Contact Form initiating
+-------------------------------------*/
+$(document).ready(function() {
+    var contactForm = $('#contact-form');
+    if (contactForm.length) {
+        contactForm.validator().on('submit', function(e) {
+            var $this = $(this),
+                $target = contactForm.find('.form-response');
+            if (e.isDefaultPrevented()) {
+                $target.html("<div class='alert alert-danger'><p>Please select all required fields.</p></div>");
+            } else {
+                e.preventDefault(); // Prevent form submission
+                // Serialize the form data
+                var formData = contactForm.serialize();
+                
+                // Log the serialized form data to the console
+                console.log("Form Data Sent: ", formData);
+
+                $.ajax({
+                    url: "php/form-process.php",
+                    type: "POST",
+                    data: formData,
+                    beforeSend: function() {
+                        $target.html("<div class='alert alert-info'><p>Loading ...</p></div>");
+                    },
+                    success: function(response) 
+                    {
+                        try {
+                            var res = JSON.parse(response);
+                            console.log(res);
+
+                            if (res.success) {
+                                $this[0].reset();
+                                $target.html("<div class='alert alert-success'><p>Message has been sent successfully.</p></div>");
+                            } else {
+                                var messages = res.message.map(function(message) {
+                                    return "<p>" + message + "</p>";
+                                }).join('');
+
+                                $target.html("<div class='alert alert-danger'><p>" + messages + "</p></div>");
+                            }
+                        } catch (e) {
+                            console.error("Parsing error:", e);
+                            $target.html("<div class='alert alert-danger'><p>An error occurred. Please try again later.</p></div>");
+                        }
+                        /* //console.log(response);
+                        var res = response;
+                        console.log(res);
+                        if (res.success) {
+                            $this[0].reset();
+                            $target.html("<div class='alert alert-success'><p>Message has been sent successfully.</p></div>");
+                        } else {
+                            if (res.message.length) {
+                                var messages = "";
+                                res.message.forEach(function(message) {
+                                    messages += "<p>" + message + "</p>";
+                                });
+                                $target.html("<div class='alert alert-danger'>" + messages + "</div>");
+                            }
+                        } */
+                    },
+                    error: function(xhr, status, error) {
+                        console.error(xhr);
+                        $target.html("<div class='alert alert-danger'><p>Error: " + status + "</p></div>");
+                    }
+                });
+                return false;
+            }
+        });
+    }
+});
+
+
